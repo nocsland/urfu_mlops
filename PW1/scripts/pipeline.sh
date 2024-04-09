5f7f3af569df037c4e9fdd14e6f43a4a4bb5cdad
Anton Vlasov <nocsland@gmail.com>
2024-04-09 22:26:40 +0400

    Refactoring pipeline.sh, update README.md

--- PW1/scripts/pipeline.sh
+++ PW1/scripts/pipeline.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
 
+# Проверяем наличие Python
+if ! command -v python3 &> /dev/null; then
+    echo "Python не найден. Установите Python для продолжения."
+    exit 1
+fi
+
 # Проверяем, запущен ли скрипт непосредственно из каталога scripts
 if [[ "$(basename "$(pwd)")" != "scripts" ]]; then
-  echo "Скрипт должен быть запущен из каталога где он расположен (scripts)"
+  echo "Скрипт должен быть запущен из каталога, где он расположен (scripts)"
   exit 1
+fi
+
+# Проверяем наличие файла requirements.txt
+if [ ! -f "../requirements.txt" ]; then
+    echo "Файл requirements.txt не найден."
+    exit 1
 fi
 
 # Проверка активации виртуального окружения
@@ -11,28 +23,29 @@ if [[ -z "${VIRTUAL_ENV}" ]]; then
    echo "Виртуальное окружение не активировано"
 
    # Создание виртуального окружения
-   python3 -m venv ../../venv
+   python3 -m venv ../../venv || { echo "Ошибка при создании виртуального окружения."; exit 1; }
 
    # Активация виртуального окружения
-   source ../../venv/bin/activate
-   echo "Проверяю и устанавливаю зависимости"
-   pip install -r ../requirements.txt
+   source ../../venv/bin/activate || { echo "Ошибка при активации виртуального окружения."; exit 1; }
+
+   echo "Устанавливаю зависимости"
+   pip install -r ../requirements.bak || { echo "Ошибка при установке зависимостей."; exit 1; }
 else
    echo "Виртуальное окружение активировано"
 fi
 
-cd ../src || exit
+cd ../src || { echo "Не удалось перейти в каталог src"; exit 1; }
 
 echo "Запускаю пайплайн"
 
 # Запуск создания данных
-python data_creation.py
+python data_creation.py || { echo "Ошибка при выполнении data_creation.py"; exit 1; }
 
 # Запуск предобработки данных
-python model_preprocessing.py
+python model_preprocessing.py || { echo "Ошибка при выполнении model_preprocessing.py"; exit 1; }
 
 # Запуск подготовки и обучения модели
-python model_preparation.py
+python model_preparation.py || { echo "Ошибка при выполнении model_preparation.py"; exit 1; }
 
 # Запуск тестирования модели
-python model_testing.py
+python model_testing.py || { echo "Ошибка при выполнении model_testing.py"; exit 1; }
