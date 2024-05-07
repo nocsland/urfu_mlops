ba8b63af83ac62515cbdfb000d798e36993b9692
Anton Vlasov <nocsland@gmail.com>
2024-05-07 22:32:26 +0400

    Update translator. update samples ip.

--- translator/samples/en-ru.py
+++ translator/samples/en-ru.py
@@ -1,6 +1,6 @@
 import requests
 
-url = "http://127.0.0.1:8000/translate/en-to-ru/"
+url = "http://91.185.85.43:8000/translate/en-to-ru/"
 payload = {"text": "The quick brown fox jumps over the lazy dog."}
 response = requests.post(url, json=payload)
 print(response.json())
