ca55e87448b6195f00bb579fc6323790b5a6cd11
nadezhdasopilova <nad59647788@gmail.com>
2024-05-09 21:23:11 +0300

    Добавлены тесты на качество данных

---
+++ translator/tests/test_data.py
@@ -0,0 +1,54 @@
+from ast import literal_eval
+import pandas as pd
+import re
+
+test_data = pd.read_csv(
+    filepath_or_buffer="translator/data/datasets/dataset.csv"
+)
+
+
+# Проверка наличия всех необходимых ключей
+def test_keys_existence():
+    train = test_data["train"]
+    for el in train:
+        el_dict = dict(literal_eval(el))
+        assert "id" in el_dict, "Key 'id' is missing"
+        assert "translation" in el_dict, "Key 'translation' is missing"
+        assert "en" in el_dict["translation"], "English translation is missing"
+        assert "ru" in el_dict["translation"], "Russian translation is missing"
+
+
+# Проверка типов данных: значения для каждого ключа имеют ожидаемый тип
+def test_data_types():
+    train = test_data["train"]
+    for el in train:
+        el_dict = dict(literal_eval(el))
+        assert isinstance(el_dict["id"], str), "id should be a string"
+        assert isinstance(el_dict["translation"], dict), (
+            "translation should be a dictionary"
+        )
+        assert all(
+            isinstance(value, str) for value in el_dict["translation"].values()
+        ), "All translations should be strings"
+
+
+# Проверка формата ID
+def test_id_format():
+    train = test_data["train"]
+    for el in train:
+        assert re.match(r'^\d+$', dict(literal_eval(el))["id"]), \
+            "ID format is incorrect"
+
+
+# Проверка содержимого перевода
+def test_translation_content():
+    train = test_data["train"]
+    for el in train:
+        for lang, text in dict(literal_eval(el))["translation"].items():
+            assert text.strip(), f"Translation for {lang} is empty or whitespace"
+
+
+# Проверка уникальности ID
+def test_unique_ids():
+    ids = [dict(literal_eval(item))['id'] for item in test_data["train"]]
+    assert len(ids) == len(set(ids)), "IDs are not unique"
