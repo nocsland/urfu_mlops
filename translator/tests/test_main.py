4d4f8f66ffa2082a3c30e9d363a076c89dad9a78
Anton Vlasov <nocsland@gmail.com>
2024-05-10 14:18:02 +0400

    Update tests. Update decorators

--- translator/tests/test_main.py
+++ translator/tests/test_main.py
@@ -8,7 +8,8 @@ client = TestClient(app)
 @allure.feature("Translation API")
 class TestTranslation:
 
-    @allure.story("Translate Russian to English")
+    @allure.title("Test translation from Russian to English with 404 response")
+    @allure.description("Test case to verify the translation from Russian to English with a text that does not exist")
     @allure.severity(allure.severity_level.NORMAL)
     def test_translate_ru_en_404(self):
         with allure.step("Sending translation request from Russian to English"):
@@ -16,7 +17,8 @@ class TestTranslation:
         with allure.step("Verifying response status code"):
             assert response.status_code == 404
 
-    @allure.story("Translate Russian to English")
+    @allure.title("Test translation from Russian to English with 200 response")
+    @allure.description("Test case to verify the translation from Russian to English with a valid text")
     @allure.severity(allure.severity_level.NORMAL)
     def test_translate_ru_en_ok(self):
         with allure.step("Sending translation request from Russian to English"):
@@ -25,7 +27,8 @@ class TestTranslation:
             assert response.status_code == 200
             assert response.json() == {"translated_text": "Hey."}
 
-    @allure.story("Translate Russian to English")
+    @allure.title("Test translation from Russian to English with a text phrase")
+    @allure.description("Test case to verify the translation from Russian to English with a text phrase")
     @allure.severity(allure.severity_level.NORMAL)
     def test_translate_ru_en_phrase_ok(self):
         with allure.step("Sending translation request with text phrase from Russian to English"):
@@ -34,7 +37,8 @@ class TestTranslation:
             assert response.status_code == 200
             assert response.json() == {"translated_text": "Example of a text phrase."}
 
-    @allure.story("Translate Russian to English")
+    @allure.title("Test translation from Russian to English with an error")
+    @allure.description("Test case to verify the translation from Russian to English with an error in the request")
     @allure.severity(allure.severity_level.NORMAL)
     def test_translate_ru_en_error(self):
         with allure.step("Sending translation request from Russian to English"):
@@ -43,7 +47,8 @@ class TestTranslation:
             assert response.status_code == 200
             assert not response.json() == {"translation_text": "Hi."}
 
-    @allure.story("Translate Russian to English")
+    @allure.title("Test translation from Russian to English with an incorrect request")
+    @allure.description("Test case to verify the translation from Russian to English with incorrect request data")
     @allure.severity(allure.severity_level.NORMAL)
     def test_translate_ru_en_not_200(self):
         with allure.step("Sending translation request from Russian to English with incorrect data"):
@@ -51,7 +56,8 @@ class TestTranslation:
         with allure.step("Verifying response status code"):
             assert response.status_code == 422
 
-    @allure.story("Translate English to Russian")
+    @allure.title("Test translation from English to Russian with 404 response")
+    @allure.description("Test case to verify the translation from English to Russian with a text that does not exist")
     @allure.severity(allure.severity_level.NORMAL)
     def test_translate_en_ru_404(self):
         with allure.step("Sending translation request from English to Russian"):
@@ -59,7 +65,8 @@ class TestTranslation:
         with allure.step("Verifying response status code"):
             assert response.status_code == 404
 
-    @allure.story("Translate English to Russian")
+    @allure.title("Test translation from English to Russian with 200 response")
+    @allure.description("Test case to verify the translation from English to Russian with a valid text")
     @allure.severity(allure.severity_level.NORMAL)
     def test_translate_en_ru_ok(self):
         with allure.step("Sending translation request from English to Russian"):
@@ -68,7 +75,8 @@ class TestTranslation:
             assert response.status_code == 200
             assert response.json() == {"translated_text": "Привет."}
 
-    @allure.story("Translate English to Russian")
+    @allure.title("Test translation from English to Russian with a text phrase")
+    @allure.description("Test case to verify the translation from English to Russian with a text phrase")
     @allure.severity(allure.severity_level.NORMAL)
     def test_translate_en_ru_phrase_ok(self):
         with allure.step("Sending translation request with text phrase from English to Russian"):
@@ -77,7 +85,8 @@ class TestTranslation:
             assert response.status_code == 200
             assert response.json() == {"translated_text": "Пример формулировки текста."}
 
-    @allure.story("Translate English to Russian")
+    @allure.title("Test translation from English to Russian with an error")
+    @allure.description("Test case to verify the translation from English to Russian with an error in the request")
     @allure.severity(allure.severity_level.NORMAL)
     def test_translate_en_ru_error(self):
         with allure.step("Sending translation request from English to Russian"):
@@ -86,7 +95,8 @@ class TestTranslation:
             assert response.status_code == 200
             assert not response.json() == {"translated_text": "Не Привет."}
 
-    @allure.story("Translate English to Russian")
+    @allure.title("Test translation from English to Russian with an incorrect request")
+    @allure.description("Test case to verify the translation from English to Russian with incorrect request data")
     @allure.severity(allure.severity_level.NORMAL)
     def test_translate_en_ru_error_not_200(self):
         with allure.step("Sending translation request from English to Russian with incorrect data"):
